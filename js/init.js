c81fc0e0c610c9de263d87a2536e612ac6dcd593
ProyectoFinal1000 <ProyectoFinal1000@gmail.com>
2021-06-15 18:49:41 -0500

    Cambio

--- js/init.js
+++ js/init.js
@@ -7,6 +7,7 @@
  * quedar igual que en tu
  * servidor. */
 // @ts-ignore
+//Hola
 firebase.initializeApp({
   apiKey: "AIzaSyAGj3DqTtchpbizn8Zzvo0zzY17bNWkMBI",
     authDomain: "proyectofinal10000.firebaseapp.com",
