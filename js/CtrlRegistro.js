63739ede58a1278984b1ee91f8f51e1d7ad95ce9
ProyectoFinal1000 <ProyectoFinal1000@gmail.com>
2021-06-25 10:02:54 -0500

    Cambio

--- js/CtrlRegistro.js
+++ js/CtrlRegistro.js
@@ -1,14 +1,24 @@
 "use strict";
-var forma = document.getElementById("forma"),
-campos = ["raza", "edad", "telefono", "email", "adoptado", "fecha"],
-salidas = ["salidaNombre", "SalidaEdad", "salidaTelefono", "salidaEmail", "salidaAdoptado", "salidaFecha"];
-
-forma.addEventListener("submit", procesa, false);
+var FMT_NUMERO = "0,0.00",
+ FMT_PORCENTAJE="%0",
+ forma = document.getElementById("forma"),
+registro = document.getElementById("salidaRegistro"),
+precio = document.getElementById("salidaPrecio"),
+descuento = document.getElementById("salidaDescuento"),
+extraviada= document.getElementById("salidaExtraviada"),
+salidaUtilidad = document.getElementById("salidaUtilidad"),
+salidaUDesc = document.getElementById("salidaUDesc"),
+salidaRobada = document.getElementById("salidaRobada");
 
 function procesa(){
-    for (var i = 0, longitud = campos.length; i < longitud; i++) {
-    var campo = forma[campos[i]];
-    var salida = document.getElementById(salidas[i]);
-    salida.value = campo.value;
-  }
-}
+    var operacion1 = numeral().unformat(forma["registro"].value),
+        operacion2 = numeral().unformat(forma["precio"].value),
+        operacion3= numeral().unformat(forma["descuento"].value),
+        operacion4= numeral().unformat(forma["extraviada"].value);
+        var uti = (operacion1)*(operacion2),
+            desc=(operacion1)*(operacion2)*(operacion3),
+            rob=(operacion1)-(operacion4)*(operacion2);
+        salidaUtilidad.value =numeral(uti).format(FMT_NUMERO); 
+        salidaUDesc.value =numeral(desc).format(FMT_NUMERO);
+        salidaRobada.value =numeral(rob).format(FMT_NUMERO);
+    }
