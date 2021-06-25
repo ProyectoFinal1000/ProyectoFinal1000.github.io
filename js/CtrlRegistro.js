a2beeb79947af4a9912f0f3df3a3c64ae0a8d07b
ProyectoFinal1000 <ProyectoFinal1000@gmail.com>
2021-06-24 21:08:35 -0500

    Cambio

--- js/CtrlRegistro.js
+++ js/CtrlRegistro.js
@@ -1,12 +1,21 @@
 "use strict";
 var forma = document.getElementById("forma"),
+txtFecha = forma["fecha"],
 campos = ["nombre", "apellido", "telefono", "email", "adoptado"],
+salidaFecha = document.getElementById("salidaFecha"),
 salidas = ["salidaNombre", "salidaApellido", "salidaTelefono", "salidaEmail", "salidaAdoptado"];
+
 forma.addEventListener("submit", procesa, false);
+
 function procesa(){
-  for (var i = 0, longitud = campos.length; i < longitud; i++) {
+    var fechaValueAsDate = txtFecha.valueAsDate,
+    fecha = new Date(txtFecha.value),
+    for (var i = 0, longitud = campos.length; i < longitud; i++) {
     var campo = forma[campos[i]];
     var salida = document.getElementById(salidas[i]);
     salida.value = campo.value;
+    salidaFecha.textContent = txtFecha.type + " | "+txtFecha.value + "|"
+    +fecha.toGMTString() +
+    (fechaValueAsDate ? "|valueAsDate: "+fechaValueAsDate.toGMTString(): "");
   }
 }
