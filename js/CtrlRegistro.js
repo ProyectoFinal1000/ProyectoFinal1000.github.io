c5ad1f1763c7ecbe1278e3d81d1c84e6ed48aa47
ProyectoFinal1000 <ProyectoFinal1000@gmail.com>
2021-06-24 23:20:32 -0500

    Cambio

--- js/CtrlRegistro.js
+++ js/CtrlRegistro.js
@@ -1,3 +1,19 @@
+import {
+  getAuth
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+import {
+  checksRoles,
+  guardaUsuario,
+  selectPasatiempos
+} from "./usuarios.js";
+
 "use strict";
 var forma = document.getElementById("forma"),
 campos = ["raza", "telefono", "email", "adoptado", "fecha"],
@@ -11,4 +27,14 @@ function procesa(){
     var salida = document.getElementById(salidas[i]);
     salida.value = campo.value;
   }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  const formData =
+    new FormData(forma);
+  const id = getString(
+    formData, "raza").trim();
+  await guardaUsuario(evt,
+    formData, id);
 }
