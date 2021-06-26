9a9f3306917a017ebcc225f9c13184ebf9a86b72
ProyectoFinal1000 <85970607+ProyectoFinal1000@users.noreply.github.com>
2021-06-26 12:46:42 -0500

    Update CtrlRegistro.js

--- js/CtrlRegistro.js
+++ js/CtrlRegistro.js
@@ -2,24 +2,24 @@
 var FMT_NUMERO = "0,0.00",
  FMT_PORCENTAJE="%0",
  forma = document.getElementById("forma"),
-salidaOp1 = document.getElementById("salidaOp1"),
-salidaOp2 = document.getElementById("salidaOp2"),
-salidaOp3 = document.getElementById("salidaOp3"),
-salidaOp5= document.getElementById("salidaOp4"),
-salidaIMC = document.getElementById("salidaIMC"),
-salidaPorcentaje = document.getElementById("salidaGrasa"),
-salidaCalorias = document.getElementById("salidaCalorias");
+salidaMerca = document.getElementById("salidaMerca"),
+salidaPrecio = document.getElementById("salidaPrecio"),
+salidaDesc = document.getElementById("salidaDesc"),
+salidaRobado= document.getElementById("salidaRobado"),
+salidaUtilidad = document.getElementById("salidaUtilidad"),
+salidaDescuento = document.getElementById("salidaDescuento"),
+salidaExtraviado = document.getElementById("salidaExtraviado");
 
 function procesa(){
-    var operador1 = numeral().unformat(forma["estatura"].value),
-        operador2 = numeral().unformat(forma["peso"].value),
-        operador5= numeral().unformat(forma["edad"].value),
-        operador3= numeral().unformat(forma["cintura"].value);
-        var cal =((operador1)*(operador2*.16)),
-            imc=((operador3/100)*(operador1)),
-            grasa=((operador5)-(operador1)*(operador2*.16));
-        salidaCalorias.value =numeral(cal).format(FMT_NUMERO); 
-        salidaIMC.value =numeral(imc).format(FMT_NUMERO);
-        salidaPorcentaje.value =numeral(grasa).format(FMT_NUMERO);
+    var operador1 = numeral().unformat(forma["mercancia"].value),
+        operador2 = numeral().unformat(forma["precio"].value),
+        operador5= numeral().unformat(forma["robo"].value),
+        operador3= numeral().unformat(forma["descuento"].value);
+        var uti =((operador1)*(operador2*.16)),
+            des=((operador3/100)*(operador1)),
+            extra=((operador5)-(operador1)*(operador2*.16));
+        salidaUtilidad.value =numeral(uti).format(FMT_NUMERO); 
+        salidaDescuento.value =numeral(des).format(FMT_NUMERO);
+        salidaExtraviado.value =numeral(extra).format(FMT_NUMERO);
     }
 
