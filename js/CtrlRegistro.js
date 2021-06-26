1e3b4e91de3d6beb7aae70529803a9cbeba246a7
ProyectoFinal1000 <85970607+ProyectoFinal1000@users.noreply.github.com>
2021-06-25 21:16:40 -0500

    Update CtrlRegistro.js

--- js/CtrlRegistro.js
+++ js/CtrlRegistro.js
@@ -5,20 +5,21 @@ var FMT_NUMERO = "0,0.00",
 salidaOp1 = document.getElementById("salidaOp1"),
 salidaOp2 = document.getElementById("salidaOp2"),
 salidaOp3 = document.getElementById("salidaOp3"),
-salidaOp5 = document.getElementById("salidaOp4"),
-salidaUtilidad = document.getElementById("salidaUtilidad"),
-salidaPorcentaje = document.getElementById("salidaUDesc"),
-salidaRobada = document.getElementById("salidaRobada");
+salidaOp5= document.getElementById("salidaOp4"),
+salidaIMC = document.getElementById("salidaIMC"),
+salidaPorcentaje = document.getElementById("salidaGrasa"),
+salidaCalorias = document.getElementById("salidaCalorias");
 
 function procesa(){
-    var operador1 = numeral().unformat(forma["registro"].value),
-        operador2 = numeral().unformat(forma["precio"].value),
-        operador5= numeral().unformat(forma["extraviada"].value),
-        operador3= numeral().unformat(forma["descuento"].value);
-        var uti = (66+(13.7*operador2) +(5*operador1)-(6.8*operador5))*1.2,
-            desc=(operador2*10000)/(operador1*operador1),
-            rob=0.64-(0.20*operador1/operador3);
-        salidaUtilidad.value =numeral(uti).format(FMT_NUMERO); 
-        salidaRobada.value =numeral(desc).format(FMT_NUMERO);
-        salidaPorcentaje.value =numeral(rob).format(FMT_NUMERO);
+    var operador1 = numeral().unformat(forma["estatura"].value),
+        operador2 = numeral().unformat(forma["peso"].value),
+        operador5= numeral().unformat(forma["edad"].value),
+        operador3= numeral().unformat(forma["cintura"].value);
+        var cal =(66+(13.7*operador2) +(5*operador1)-(6.8*operador5))*1.2,
+            imc=(operador2*10000)/(operador1*operador1),
+            grasa=0.64-(0.20*operador1/operador3);
+        salidaCalorias.value =numeral(cal).format(FMT_NUMERO); 
+        salidaIMC.value =numeral(imc).format(FMT_NUMERO);
+        salidaPorcentaje.value =numeral(grasa).format(FMT_PORCENTAJE);
     }
+
