bd2a805d8dfe41b25a7610cd969404aba0178882
ProyectoFinal1000 <85970607+ProyectoFinal1000@users.noreply.github.com>
2021-06-26 10:25:57 -0500

    Update CtrlChat.js

--- js/CtrlChat.js
+++ js/CtrlChat.js
@@ -23,9 +23,7 @@ const lista = document.
 getAuth().onAuthStateChanged(
   protege, muestraError);
 
-/** @param {import(
-    "../lib/tiposFire.js").User}
-    usuario */
+
 async function protege(usuario) {
   if (tieneRol(usuario,
     ["Cliente"])) {
@@ -36,9 +34,7 @@ async function protege(usuario) {
   }
 }
 
-/** Agrega un usuario a la base de
- * datos.
- * @param {Event} evt */
+
 async function agrega(evt) {
   try {
     evt.preventDefault();
@@ -59,9 +55,7 @@ async function agrega(evt) {
       texto,
       timestamp
     };
-    /* El modelo se agrega a
-     * la colección
-     * "Mensaje". */
+
     await daoMensaje.add(modelo);
     forma.texto.value = "";
   } catch (e) {
@@ -69,57 +63,24 @@ async function agrega(evt) {
   }
 }
 
-/** Muestra los mensajes
- * almacenados en la collection
- * "Mensaje". Se actualiza
- * automáticamente. */
+
 function consulta() {
-  /* Consulta que se actualiza
-   * automáticamente. Pide todos
-   * los registros de la colección
-   *  "Mensaje"
-   * ordenados por el campo
-   *  "timestamp"
-   * de forma
-   *  descendente. */
+
   daoMensaje.
     orderBy("timestamp", "desc").
     onSnapshot(
       htmlLista, errConsulta);
 }
 
-/** Muestra los datos enviados por
- * el servidor.
- * Si los datos cambian en el
- * servidor, se vuelve a invocar
- * esta función y recibe los datos
- * actualizados.
- * @param {import(
-    "../lib/tiposFire.js").
-    QuerySnapshot} snap estructura
- *    parecida a un Array, que
- *    contiene una copia de los
- *    datos del servidor.
- */
+
 function htmlLista(snap) {
   let html = "";
   if (snap.size > 0) {
-    /* Cuando el número de
-     * documentos devueltos por la
-     * consulta es mayor que 0,
-     * revisa uno por uno los
-     * documentos de la consulta y
-     * los muestra. El iterador
-     * "doc" apunta a un
-     * documento de la base
-     * de datos. */
+  
     snap.forEach(doc =>
       html += htmlFila(doc));
   } else {
-    /* Cuando el número de
-     * documentos devueltos por la
-     * consulta es igual a 0,
-     * agrega un texto HTML. */
+
     html += /* html */
       `<li class="vacio">
         -- No hay mensajes
@@ -129,22 +90,11 @@ function htmlLista(snap) {
   lista.innerHTML = html;
 }
 
-/** Agrega el texto HTML
- * que corresponde a un
- * documento de un mensaje.
- * @param {import(
-    "../lib/tiposFire.js").
-    DocumentSnapshot} doc */
+
 function htmlFila(doc) {
-  /** Recupera los datos del
-   * documento.
-   * @type {import("./tipos.js").
-                      Mensaje} */
+ 
   const data = doc.data();
-  /* Agrega un li con los datos
-   * del documento, los cuales se
-   * codifican para evitar
-   * inyección de código. */
+
   return ( /* html */
     `<li class="fila">
       <strong class="primario">
@@ -156,14 +106,7 @@ function htmlFila(doc) {
     </li>`);
 }
 
-/** Función que se invoca cuando
- * hay un error al recuperar los
- * mensajes y muestra el error. Al
- * invocar esta función, la
- * conexión se cancela, por lo
- * cual intenta conectarse otra
- * vez.
- * @param {Error} e */
+
 function errConsulta(e) {
   muestraError(e);
   // Intenta conectarse otra vez.
