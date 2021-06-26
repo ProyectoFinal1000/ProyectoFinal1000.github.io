a54dbb861732bc6c09279285a9b9bb8a6792af4a
ProyectoFinal1000 <85970607+ProyectoFinal1000@users.noreply.github.com>
2021-06-26 10:33:04 -0500

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -14,7 +14,7 @@ class MiNav extends HTMLElement {
       `<ul>
         <li>
           <a href="index.html">
-            SESION</a>
+            Sesion</a>
         </li>
       </ul>`;
     this.ul =
